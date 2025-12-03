ca41ad4ec9303ba64a8b79747d3d6789cf6e31e2
Sunmi-CS <sunmicasano@gmail.com>
2025-12-03 14:40:53 -0500

    Editado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,12 +14,6 @@ export default function Home(){
       <div className="text-center mb-4">
         <h1 className="display-6">explora animes</h1>
         <p className="lead">grid con datos desde la API de Jikan</p>
-        <img
-          src="https://i.imgur.com/5QfT5sQ.png"
-          alt="anime hero"
-          style={{ maxWidth: 420 }}
-          className="img-fluid rounded"
-        />
       </div>
 
       <h5 className="mb-3">Selección (6 elementos)</h5>
