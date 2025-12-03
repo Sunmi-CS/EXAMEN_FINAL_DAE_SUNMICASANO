bcbcb3a3017c25a38919fd1e4b9bddc09e46625f
Sunmi-CS <sunmicasano@gmail.com>
2025-12-03 14:17:28 -0500

    Modificacion del footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-light text-center py-3 mt-4">
+      <div className="container">
+        <small>Examen Final - SPA Anime SUNMI CASANO SUAREZ</small>
+      </div>
+    </footer>
+  );
+}
